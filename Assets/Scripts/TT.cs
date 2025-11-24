11026f4c3395d9593cea3c500a4eeb889155a8ac
Henry <hello264987@gmail.com>
2025-11-24 10:40:00 +0800

    補

--- Assets/Scripts/TT.cs
+++ Assets/Scripts/TT.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TT : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.qKey.wasPressedThisFrame)
+        {
+            Debug.Log("Q key was pressed");
+        }
     }
 }
