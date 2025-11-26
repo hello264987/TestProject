b9cb5cbd680af7e8eb87d474c35dc32d7c7c6b4b
Henry <hello264987@gmail.com>
2025-11-26 15:02:04 +0800

    練習UniTask

--- Assets/Scripts/TT.cs
+++ Assets/Scripts/TT.cs
@@ -1,11 +1,12 @@
+using System.Text;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class TT : MonoBehaviour
 {
+
     void Start()
     {
-        
     }
 
     void Update()
@@ -13,6 +14,16 @@ public class TT : MonoBehaviour
         if (Keyboard.current.qKey.wasPressedThisFrame)
         {
             Debug.Log("Q key was pressed");
+        }
+
+        if (Keyboard.current.wKey.wasPressedThisFrame)
+        {
+            int cmd = 123;
+            // 使用 StringBuilder 來構建字符串(這啥炫技的寫法==)
+            Debug.Log(new StringBuilder("Reserved CMD Cannot Be Used: ").Append(cmd).ToString());
+
+
+            Debug.Log("w key was pressed");
         }
     }
 }
---
+++ Assets/Scripts/UniTask_One.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class UniTask_One : MonoBehaviour
+{
+    //基礎用法
+    //延時操作
+    //UniTask.Delay 延遲幾秒：注意方法的重載及參數作用，是否受時間縮放影響
+    //UniTask.DelayFrame 延遲幾幀
+    //UniTask.NextFrame 等待一幀
+
+    //底下先不用管，涉及到其他操作
+    //UniTask.Yield 等待一幀 用於處理調回主線程用 可以用於將處理調回主線程用。 例如yield之前是在其他線程跑，yield之後回到主線程跑
+    //默認是update循環。通過變更loop的類型，能切換之後代碼的運行時機
+
+    async void Start()
+    {
+        Debug.Log("Start "+Time.frameCount);
+
+        //await UniTask.DelayFrame(100);
+        await UniTask.NextFrame();
+
+        Debug.Log("End "+Time.frameCount);
+    }
+}
