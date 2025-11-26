0d816441b6d6931c7f0ffcf9144934bcaeff32c4
Henry <hello264987@gmail.com>
2025-11-26 17:23:33 +0800

    UniTask.WaitUntil練習

---
+++ Assets/Scripts/UniTask_Two.cs
@@ -0,0 +1,36 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class UniTask_Two : MonoBehaviour
+{
+    public GameObject ball;
+    void Start()
+    {
+        //TODO
+        //UniTask.WaitUntil()
+        WaitUntilTest();
+        
+    }
+
+    void Update()
+    {
+        ball.transform.Translate(new Vector3(1, 0, 0)*Time.deltaTime);
+    }
+
+    async void WaitUntilTest()
+    {
+        Debug.Log("Start " + Time.time);
+        await UniTask.WaitUntil(() => isFrameThanOne());
+        Debug.Log("End " + Time.time);
+        ball.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public bool isFrameThanOne()
+    {
+        if (ball.transform.position.x > 1)
+        {
+            return true;
+        }
+        return false;
+    }
+}
